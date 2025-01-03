191f8c6041efb031d82b31d6fbddc59a3cfecf89
Kirill <onlyjesus13@icloud.com>
2025-01-03 22:48:01 +0500

    Initial Commit

---
+++ JKMoneyTests/JKMoneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  JKMoneyTests.swift
+//  JKMoneyTests
+//
+//  Created by prom1 on 03.01.2025.
+//
+
+import Testing
+@testable import JKMoney
+
+struct JKMoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
